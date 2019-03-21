7a25a88e494647b04aee54eb1c1dc9416a50f39e
Christian Riesen <chris.riesen@gmail.com>
2019-03-21 16:17:28 +0100

    run scripts in local directory?

--- scripts/deploy_notification.sh
+++ scripts/deploy_notification.sh
@@ -2,5 +2,5 @@
 
 # The true bit is to ensure even if one hook fails, the next one runs
 
-./slack.sh $SLACK_WEBHOOK || true
-./discord.sh success $DISCORD_WEBHOOK || true
+slack.sh $SLACK_WEBHOOK || true
+discord.sh success $DISCORD_WEBHOOK || true
