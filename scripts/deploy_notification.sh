7da36aa7eb89a2925d2e3a23495c89784b432c3d
Christian Riesen <chris.riesen@gmail.com>
2019-03-21 23:06:02 +0100

    Disable deploy slack notifications
    There is still build notifications anyways

--- scripts/deploy_notification.sh
+++ scripts/deploy_notification.sh
@@ -4,5 +4,6 @@ scripts_dir=`dirname $0`
 
 # The true bit is to ensure even if one hook fails, the next one runs
 
-$scripts_dir/slack.sh $SLACK_WEBHOOK || true
+# Slack notification has some issues, needs more debug
+#$scripts_dir/slack.sh $SLACK_WEBHOOK || true
 $scripts_dir/discord.sh success $DISCORD_WEBHOOK || true
