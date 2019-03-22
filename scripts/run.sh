91c4cec99055bac672366f0b4256786d23248f43
Christian Riesen <chris.riesen@gmail.com>
2019-03-22 16:05:15 +0100

    Whitespace

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 rm -rf /vagrant/output_dev/
+
 until /vagrant/vendor/bin/sculpin generate --watch --server; do
   echo Generation interrupted, cleaning directory and restarting generation...
   rm -rf /vagrant/output_dev/
